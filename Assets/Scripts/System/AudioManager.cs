7f13e69e56f7bd4ac717f8519fa370bdc95ea373
StevenLilley <steven.b.lilley@gmail.com>
2025-05-09 22:44:19 -0400

    Prevent multilple audio managers from existing if player returns to title screen.

--- Assets/Scripts/System/AudioManager.cs
+++ Assets/Scripts/System/AudioManager.cs
@@ -5,8 +5,18 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip backgroundMusic;
 
+    private static AudioManager Instance;
+
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         DontDestroyOnLoad(this.gameObject);
     }
 }
